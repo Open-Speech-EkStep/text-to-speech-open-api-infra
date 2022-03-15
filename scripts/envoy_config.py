426dad62023c1c426e6615a64969f086b760e8c9
ankit-thoughtworks <ankit.katiyar@thoughtworks.com>
2022-03-15 16:38:39 +0530

    |Ankit, Soujyo| update print statement

--- scripts/envoy_config.py
+++ scripts/envoy_config.py
@@ -25,8 +25,13 @@ class EnvoyConfig:
         else:
             process = "upgrade"
 
-        command = "helm {0} --timeout 180s {1} {2} --namespace {3} --set envoyAdmin.enabled='{4}'".format(process, self.release_name,
-                                                                           self.helm_chart_path, namespace, enable_envoy_admin)
+        command = "helm {0} --timeout 180s {1} {2} --namespace {3} --set envoyAdmin.enabled='{4}'" \
+            .format(process,
+                    self.release_name,
+                    self.helm_chart_path,
+                    namespace,
+                    enable_envoy_admin)
+
         cmd_runner(command, "Envoy")
 
 
